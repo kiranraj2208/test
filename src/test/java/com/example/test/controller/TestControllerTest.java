97e6dca468de2ae650f591d68a1239a9c430e6cc
kiranraj2208 <kiranraj2208@gmail.com>
2021-12-30 16:47:02 +0530

    removed testcases

--- src/test/java/com/example/test/controller/TestControllerTest.java
+++ src/test/java/com/example/test/controller/TestControllerTest.java
@@ -15,28 +15,28 @@ class TestControllerTest {
         testController = new TestController();
     }
 
-    @Test
-    void getStatus() {
-        Assertions.assertEquals("Active", testController.getStatus());
-    }
-
-    @Test
-    void getResponse() {
-        Assertions.assertNotNull(testController.getResponse());
-    }
-
-    @Test
-    void getErrorResponse() {
-        Assertions.assertNotNull(testController.getErrorResponse());
-    }
-
-    @Test
-    void getErrorResponse1() {
-        Assertions.assertNotNull(testController.getErrorResponse1());
-    }
-
-    @Test
-    void getLog() {
-        Assertions.assertNotNull(testController.getLog());
-    }
+//    @Test
+//    void getStatus() {
+//        Assertions.assertEquals("Active", testController.getStatus());
+//    }
+//
+//    @Test
+//    void getResponse() {
+//        Assertions.assertNotNull(testController.getResponse());
+//    }
+//
+//    @Test
+//    void getErrorResponse() {
+//        Assertions.assertNotNull(testController.getErrorResponse());
+//    }
+//
+//    @Test
+//    void getErrorResponse1() {
+//        Assertions.assertNotNull(testController.getErrorResponse1());
+//    }
+//
+//    @Test
+//    void getLog() {
+//        Assertions.assertNotNull(testController.getLog());
+//    }
 }
