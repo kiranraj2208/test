7958f9e28fed5bfc82ec19d42e00a69180e2db9f
kiranraj2208 <kiranraj2208@gmail.com>
2021-12-30 16:08:04 +0530

    added jacoco plugin

---
+++ src/test/java/com/example/test/controller/TestControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.test.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestControllerTest {
+
+    TestController testController;
+
+    @BeforeEach
+    void setUp() {
+        testController = new TestController();
+    }
+
+    @Test
+    void getStatus() {
+        Assertions.assertEquals("Active", testController.getStatus());
+    }
+
+    @Test
+    void getResponse() {
+        Assertions.assertNotNull(testController.getResponse());
+    }
+
+    @Test
+    void getErrorResponse() {
+        Assertions.assertNotNull(testController.getErrorResponse());
+    }
+
+    @Test
+    void getErrorResponse1() {
+        Assertions.assertNotNull(testController.getErrorResponse1());
+    }
+
+    @Test
+    void getLog() {
+        Assertions.assertNotNull(testController.getLog());
+    }
+}
